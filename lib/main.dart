ca1b3b550c9dbbae925f55a3e6980f4f5e1d9e74
Gregor Zurowski <gregor@zurowski.org>
2023-11-11 17:42:32 +0100

    Wrap with change notifier

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return ChangeNotifierProvider(
+      create: (_) => ObjectProvider(),
+      child: const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello World!'),
+          ),
         ),
       ),
     );
