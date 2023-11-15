2030dccd5a201ce62dc50ad2eb1b5a9e04f35e31
Gregor Zurowski <gregor@zurowski.org>
2023-11-15 11:12:38 +0100

    Restructure main

--- stateful_app/lib/main.dart
+++ stateful_app/lib/main.dart
@@ -10,10 +10,19 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
       ),
     );
   }
